d6becb11c1af4a8517e75936512b0bcf369e0de3
Sidharth Shankar <sidharth.shankar@mountblue.tech>
2020-03-14 15:56:19 +0530

    all code till api creation and using eslint

---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const dbCall = require('./dbQuery');
+const bodyParser = require('body-parser')
+const app = express();
+app.use(bodyParser.json());
+app.get('/api/directors', (req, res) => {
+    dbCall.getAllData('select * from director')
+    .then( dbData => {
+        res.status(200).json(dbData);
+    })
+})
+
+app.get('/api/directors/:id', (req, res) => {
+    dbCall.getAllData(`SELECT * FROM director WHERE director_id = ${req.params.id}`)
+    .then( director => {
+        res.status(200).json(director);
+    })
+})
+
+app.post('/api/directors', (req, res) => {
+    dbCall.addNewEntry(`INSERT INTO director(director_name) VALUES('${req.body.director_name}')`)
+    .then( () => {
+        res.status(200).send("added Doctors into doctor table");
+    })
+    .catch(err => console.log(err.stack));
+})
+
+app.put('/api/directors/:id', (req, res) => {
+    dbCall.updateEntry(`UPDATE director SET director_name = '${req.body.director_name}' where director_id = ${req.params.id}`)
+    .then( () => {
+        res.status(200).send("updated entry for given id");
+    })
+    .catch(error => console.log(error.stack));
+})
+
+app.delete('/api/directors/:id', (req, res) => {
+    dbCall.updateEntry(`delete from director where director_id = ${req.params.id}`)
+    .then( () => {
+        res.status(200).send("deleted entry from director for given ID");
+    })
+    .catch(error => console.log(error.stack));
+})
+
+// endpoints for movies querry
+app.get('/api/movies', (req, res) => {
+    dbCall.getAllData('select * from movies')
+    .then( dbData => {
+        res.status(200).json(dbData);
+    })
+})
+app.get('/api/movies/:id', (req, res) => {
+    dbCall.getAllData(`SELECT * FROM movies WHERE rank = ${req.params.id}`)
+    .then( movies => {
+        res.status(200).json(movies);
+    })
+})
+
+app.post('/api/movies', (req, res) => {
+    const value = Object.values(req.body);
+    dbCall.addNewEntry('INSERT INTO movies VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)', value)
+    .then( () => {
+        res.status(200).send("added movies into movies tablel");
+    })
+    .catch(err => console.log(err.stack));
+})
+app.put('/api/movies/:id', (req, res) => {
+    dbCall.updateEntry(`UPDATE movies SET title = '${req.body.name}' where rank = ${req.params.id}`)
+    .then( () => {
+        res.status(200).send("updated Movies for given ID");
+    })
+    .catch(error => console.log(error.stack));
+})
+app.delete('/api/movies/:id', (req, res) => {
+    dbCall.updateEntry(`delete from movies where rank = ${req.params.id}`)
+    .then(() => {
+        res.status(200).send("deleted entry from movies for given ID");
+    })
+    .catch(error => console.log(error.stack));
+})
+
+app.listen(8080);
---
+++ dbQuery.js
@@ -0,0 +1,125 @@
+const { Pool } = require('pg');
+const data = require('./data/movies.json');
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "movies_data",
+    password: "postgres",
+    port: 5432,
+    max: 20
+})
+function insertDirector() {
+    return pool
+  .connect()
+  .then(client => {
+    return client
+      .query(`create table director(director_id serial primary key, director_name varchar(35))`)
+      .then(() => {
+        let id = 1;
+        const result = data.reduce((emptyObj, arrayObj) => {
+            if(!emptyObj[arrayObj.Director]) {
+                emptyObj[arrayObj.Director] = id++;
+                client.query(`INSERT INTO director (director_name) VALUES('${arrayObj.Director}')`)
+            }
+            return emptyObj;
+        }, {})
+        client.release()
+        return result;
+      })
+      .catch(err => {
+        client.release()
+        console.log(err.stack)
+      })
+    })
+}
+function insertMovies(dirObj) {
+    pool
+  .connect()
+  .then(client => {
+    return client
+      .query(`create table IF NOT EXISTS movies(rank integer primary key, title varchar(70), description varchar(500),
+             runtime integer, genre varchar(30), rating numeric, metascore varchar(10), votes integer, gross_earning varchar(15), 
+             actor varchar(50), year integer, director_id integer)`)
+      .then(() => {
+        data.forEach((arrayObj) => {
+            let value = Object.values(arrayObj);
+            value.splice(9, 1);
+            value.push(dirObj[arrayObj.Director]);
+            console.log(value);
+            client.query('INSERT INTO movies VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)',value)
+        })
+        client.release();
+      })
+      .catch(err => {
+        client.release()
+        console.log(err.stack)
+      })
+    })
+}
+
+async function dbCall() {
+    try {
+        let dirObj = {};
+        dirObj = await insertDirector(dirObj);
+        await insertMovies(dirObj);
+    } catch(error) {
+        console.log(error.stack);
+    }
+}
+dbCall();
+
+async function getAllData(query) {
+ const result = await pool
+  .connect()
+  .then(client => {
+    return client
+      .query(query)
+      .then(res => {
+        client.release();
+        return res.rows;
+      })
+      .catch(err => {
+        client.release()
+        console.log(err.stack)
+      })
+  })
+  return result;
+}
+
+async function addNewEntry(query, value) {
+    await pool
+    .connect()
+    .then(client => {
+      return client
+        .query(query, value)
+        .then(() => {
+          client.release();
+        })
+        .catch(err => {
+          client.release()
+          console.log(err.stack)
+        })
+    })
+}
+async function updateEntry(query) {
+    await pool
+    .connect()
+    .then(client => {
+      return client
+        .query(query)
+        .then(() => {
+          client.release();
+        })
+        .catch(err => {
+          client.release()
+          console.log(err.stack)
+        })
+    })
+}
+module.exports = {
+    getAllData,
+    addNewEntry,
+    updateEntry
+}
+
+
